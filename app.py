63190df71afa6cce9efeef708457bc13c5002c96
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-11-06 21:01:19 +0900

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"<h1>ホーム</h1>"
+    return render_template('index.html')
 
-@app.route('/<drink>')
-def hello(drink):
+@app.route('/<string:drink>')
+def drink(drink):
     return f"<h1>好きな飲み物は{drink}</h1>"
+
+@app.route('/<int:number>')
+def number(number):
+    return f"<h1>好きな正数は{number}</h1>"
